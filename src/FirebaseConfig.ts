e8b88c7599fd3ad2507b4bfacd9da075462de9d1
Lorenzo Zanotto <info@lorenzozanotto.com>
2022-11-23 18:44:10 +0100

    add placeholder file configuration file

---
+++ src/FirebaseConfig.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_DEV_CONFIG = {
+  apiKey: '{il-tuo-valore-di-firebase-qui-dentro}',
+  authDomain: '{il-tuo-valore-di-firebase-qui-dentro}',
+  databaseURL: '{il-tuo-valore-di-firebase-qui-dentro}',
+  projectId: '{il-tuo-valore-di-firebase-qui-dentro}',
+  storageBucket: '{il-tuo-valore-di-firebase-qui-dentro}',
+  messagingSenderId: '{il-tuo-valore-di-firebase-qui-dentro}',
+  appId: '{il-tuo-valore-di-firebase-qui-dentro}'
+};
